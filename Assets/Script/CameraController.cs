e050ea1580e652a00e170f058f5c86aecdf8d463
Burak Karademir <94866565+burakkarademirdeveloper@users.noreply.github.com>
2023-11-18 11:59:04 +0300

    add: camera controller

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _minXValue;
+    [SerializeField] private float _maxXValue;
+
+    private Vector2 _mouseStart;
+
+    private void Update()
+    {
+        CameraMove();
+    }
+
+    private void CameraMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _mouseStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector2 mouseEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = _mouseStart - mouseEnd;
+            Vector3 newPosition = transform.position + new Vector3(direction.x, 0, 0);
+            newPosition.x = Mathf.Clamp(newPosition.x, _minXValue, _maxXValue);
+            transform.position = newPosition;
+        }
+    }
+}
